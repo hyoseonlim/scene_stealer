5d9100343a359e62bad6eea19b40f91f03076fda
hyunjung <ariestail@kakao.com>
2024-08-17 19:03:41 +0900

    rosetta productlist

--- src/main/java/pack/repository/ProductsRepository.java
+++ src/main/java/pack/repository/ProductsRepository.java
@@ -12,9 +12,11 @@ import pack.entity.Product;
 public interface ProductsRepository extends JpaRepository<Product, Integer> {
     Product findByNo(Integer no);
     boolean existsByName(String name);  // 이름 중복 확인 메서드 추가
+
     public List<Product> findByCategory(String category);
     
     @Query("SELECT a FROM Product a WHERE a.name LIKE %:term%")
     List<Product> findByNameContaining(@Param("term") String term);
+
 
 }
