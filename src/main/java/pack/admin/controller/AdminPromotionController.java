b0762d85fa4ec780dfce48d5619be68e2bc84286
hyo <hyoseonee0729@naver.com>
2024-08-25 15:35:52 +0900

    관리자 광고 알림 파트 구현

--- src/main/java/pack/admin/controller/AdminPromotionController.java
+++ src/main/java/pack/admin/controller/AdminPromotionController.java
@@ -6,15 +6,20 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import pack.dto.AlertDto;
+import jakarta.annotation.PostConstruct;
+import pack.admin.model.AdminMainModel;
+import pack.admin.model.AdminPromotionModel;
 import pack.dto.AlertDto_a;
 import pack.dto.CouponDto;
-import pack.dto.CouponUserDto;
 import pack.dto.CouponUserDto_a;
+import pack.dto.ProductDto;
+import pack.dto.ShowDto;
 import pack.entity.Coupon;
 import pack.entity.User;
 import pack.repository.AlertsRepository;
@@ -24,6 +29,12 @@ import pack.repository.UsersRepository;
 
 @RestController
 public class AdminPromotionController {
+	
+	@Autowired
+	private AdminPromotionModel promotionDao;
+	
+	@Autowired
+	private AdminMainModel mainDao;
 	
 	@Autowired
 	private CouponsRepository couponsRepo;
@@ -37,26 +48,51 @@ public class AdminPromotionController {
 	@Autowired
 	private AlertsRepository alertsRepo;
 	
-	// 쿠폰 추가 (총 3개 테이블 처리)
+	// 쿠폰 추가 (테이블 처리: coupon, coupon_user, alerts)
 	@PostMapping("/admin/coupon")
-	public Map<String, Object> insert(@RequestBody CouponDto couponDto) {
+	public Map<String, Object> insertCoupon(@RequestBody CouponDto couponDto) {
 		// 1. 쿠폰 테이블 (쿠폰명, 할인율, 유효기간)
 		Coupon newCoupon = couponsRepo.save(CouponDto.toEntity(couponDto));
 		// 2. 쿠폰-유저 관계 테이블 (추가된 해당 쿠폰 PK, 유저 PK)	
 		CouponUserDto_a cuDto = new CouponUserDto_a();
 		cuDto.setCouponNo(newCoupon.getNo());
 		
-		List<User> userList = usersRepo.findAll();
-		List<Integer> userNoList = userList.stream().map(User::getNo).collect(Collectors.toList());
+		List<Integer> userNoList = usersRepo.findAll().stream().map(User::getNo).collect(Collectors.toList());
 		for(int i=0; i<userNoList.size(); i++) { // 전체 유저 수 만큼 반복 (동일 쿠폰을 유저만 바꿔 추가)
 			cuDto.setUserNo(userNoList.get(i));
 			couponUserRepo.save(CouponUserDto_a.toEntity(cuDto, couponsRepo, usersRepo));
 		}
 		
-		// 3. 알림 테이블 (유저 PK, 카테고리 = '프로모션', content = 쿠폰명 + 할인율 + '발급완료!'
+		// 3. 알림 테이블 (유저 PK, 카테고리 = '프로모션', content = 쿠폰명 + 할인율 + '발급완료!')
 		AlertDto_a alertDto = new AlertDto_a();
 		alertDto.setCategory("프로모션");
 		alertDto.setContent("(" + couponDto.getDiscountRate() +"% 할인!)" + couponDto.getName() + "쿠폰이 발급되었습니다.");
+		
+		for(int i=0; i<userNoList.size(); i++) { // 전체 유저 수 만큼 반복 (동일 쿠폰을 유저만 바꿔 추가)
+			alertDto.setUserNo(userNoList.get(i));
+			//  🤖🤖🔨 경로가 정해지면 여기에 <유저의 쿠폰함>으로 setPath도 해야됑 🤖🤖🔨
+			alertsRepo.save(AlertDto_a.toEntity(alertDto, usersRepo));
+		}
+		return Map.of("isSuccess", true);
+	}
+	
+	// 광고 알림 추가 전 경로 설정을 위한 자동완성
+	@GetMapping("/admin/promotion/autocomplete/{locCategory}/{term}")
+    public List<?> autocomplete(@PathVariable("locCategory") String locCategory, @PathVariable("term") String term) {
+        switch (locCategory) {
+		case "product": return term.equals("") ? promotionDao.searchProducts() : promotionDao.searchProducts(term);
+		case "show": return term.equals("") ? mainDao.searchShows() : mainDao.searchShows(term);
+		// case "character":
+		default: return List.of(); // locCategory가 예상한 값이 아닐 경우 빈 리스트를 반환
+        }
+	}
+	
+	
+	// 광고 알림 추가 (테이블 처리: alerts)
+	@PostMapping("/admin/advertise")
+	public Map<String, Object> insertAd(@RequestBody AlertDto_a alertDto) { // content, path 설정됨
+		alertDto.setCategory("프로모션");
+		List<Integer> userNoList = usersRepo.findAll().stream().map(User::getNo).collect(Collectors.toList());
 		for(int i=0; i<userNoList.size(); i++) { // 전체 유저 수 만큼 반복 (동일 쿠폰을 유저만 바꿔 추가)
 			alertDto.setUserNo(userNoList.get(i));
 			alertsRepo.save(AlertDto_a.toEntity(alertDto, usersRepo));
--- src/main/java/pack/admin/model/AdminPromotionModel.java
+++ src/main/java/pack/admin/model/AdminPromotionModel.java
@@ -1,8 +1,24 @@
 package pack.admin.model;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import pack.dto.ProductDto;
+import pack.entity.Product;
+import pack.repository.ProductsRepository;
 
 @Repository
 public class AdminPromotionModel {
-
+	@Autowired
+	private ProductsRepository productRepo;
+	
+	public List<ProductDto> searchProducts(){
+		return productRepo.findAll().stream().map(Product::toDto).toList();
+	}
+	
+	public List<ProductDto> searchProducts(String name){
+		return productRepo.findByNameContaining(name).stream().map(Product::toDto).toList();
+	}
 }
