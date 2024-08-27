1a0a92accd345ffffb5886c5ffd4e4220050c30a
kimbobo1 <bohyun9496@gmail.com>
2024-08-27 14:15:21 +0900

    plz

--- src/main/java/pack/admin/controller/AdminCommunityController.java
+++ src/main/java/pack/admin/controller/AdminCommunityController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,8 +32,9 @@ public class AdminCommunityController {
     public ResponseEntity<Page<PostDto>> getAllPosts(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
-        
-        Pageable pageable = PageRequest.of(page, size);
+
+        // 최신순으로 정렬
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "date"));
         Page<PostDto> postPage = adminCommunityModel.getAllPosts(pageable);
         
         return ResponseEntity.ok(postPage);
